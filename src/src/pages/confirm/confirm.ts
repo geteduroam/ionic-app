d7627202b671de6b5f897774ba4f97885f9575a4
CarlosFJ <cfierro@emergya.com>
2019-11-22 11:03:05 +0100

    Include:  pages: confirm, profile, institution, error  guide-style: base-content, components

---
+++ src/src/pages/confirm/confirm.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ConfirmPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-confirm',
+  templateUrl: 'confirm.html',
+})
+export class ConfirmPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+}
--- src/src/pages/welcome/welcome.ts
+++ src/src/pages/welcome/welcome.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the WelcomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NavController, NavParams } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+import { ErrorsPage } from '../errors/errors';
+import { ConfirmPage } from '../confirm/confirm';
+import { InstitutionPage } from '../institution/institution';
 
 @Component({
   selector: 'page-welcome',
@@ -17,8 +14,21 @@ export class WelcomePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad WelcomePage');
+  async navigateTo(page: string) {
+    switch (page) {
+      case 'profile':
+        await this.navCtrl.push(ProfilePage);
+        break;
+      case 'error':
+        await this.navCtrl.push(ErrorsPage);
+        break;
+      case 'confirm':
+        await this.navCtrl.push(ConfirmPage);
+        break;
+      case 'institution':
+        await this.navCtrl.push(InstitutionPage);
+        break;
+    }
   }
 
 }
