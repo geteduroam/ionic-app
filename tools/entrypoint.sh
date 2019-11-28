f244f1e053f65f5e3bb91e198d9a49a997cb97a5
Cardeniosa <julioap@gmail.com>
2019-11-28 07:30:50 +0100

    Simplifying script

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /home/gradle/myApp
-ls -la
 
 echo "executing entrypoint.sh ..."
 #rebuild npm
@@ -10,16 +9,19 @@ npm install
 
 echo "npm rebuild node-sass"
 npm rebuild node-sass
+
+echo "npm run build"
 npm run build
 
 #Check if android folder already exists to delete it
-if [-d andriod ]; then
+if [ -d "/home/gradle/myApp/andriod" ]; then
     echo "Removing android dir"
-    rm -rf android
+    rm -rf /home/gradle/myApp/andriod
 fi
 
-echo "npx cap add android"
 npm install --save @capacitor/core @capacitor/cli
+
+echo "npx cap add android"
 npx cap add android
 
 # Enter the new created android directory
@@ -31,12 +33,18 @@ cd android
 # Go to the new generated APK directory
 cd app/build/outputs/apk/debug/
 
+#Check if app-debug-aligned.apk already exists to delete it
+if [ -f "/home/gradle/myApp/andriod/app/build/outputs/apk/debug/app-debug-aligned.apk" ]; then
+    echo "Removing app-debug-aligned.apk"
+    rm -f /home/gradle/myApp/andriod/app/build/outputs/apk/debug/app-debug-aligned.apk
+fi
+
 #Align the APK
-zipalign -v -p 4 app-debug.apk app-debug-aligned.apk
+$ANDROID_HOME/zipalign -v -p 4 app-debug.apk app-debug-aligned.apk
 
 #Sign the APK
 #TODO take password as parameter
-apksigner sign --ks /release-key.jks --ks-pass pass:emergya --key-pass pass:emergya --out geteduroam.apk app-debug-aligned.apk
+$ANDROID_HOME/build-tools/28.0.3/apksigner sign --ks /release-key.jks --ks-pass pass:${PASS_PHRASE} --key-pass pass:${PASS_PHRASE} --out geteduroam.apk app-debug-aligned.apk
 
 
 
