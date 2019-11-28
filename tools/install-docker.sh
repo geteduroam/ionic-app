40c4071228df8f17f21c49c21d942d5e6b4085f0
Cardeniosa <julioap@gmail.com>
2019-11-28 07:29:34 +0100

    Removing get-docker.sh after installation

--- tools/install-docker.sh
+++ tools/install-docker.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-echo "Installing basic tools..."
-apt-get upgrade -qq
-apt-get update -qq -y
-apt install -qq -y apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+echo "Installing docker and docker-compose..."
 
+#install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
 
-echo "Installing Docker..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update -qq
-apt-get install -qq docker-ce docker-ce-cli containerd.io
-usermod -aG docker ${USER}
+rm get-docker.sh
 
-echo "Installing Docker Compose..."
-curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+#install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-
-reboot now
+sudo reboot now
