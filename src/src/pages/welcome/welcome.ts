def7a5ebfe31bf3efba1a6b9feede4cce78298e3
CarlosFJ <cfierro@emergya.com>
2019-11-29 15:19:10 +0100

    Base content and welcomePage

--- src/src/pages/welcome/welcome.ts
+++ src/src/pages/welcome/welcome.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-//TODO: REMOVE THIS NAVIGATE, AFTER IMPLEMENTS NAVIGATION FROM PAGES
-import { ProfilePage } from '../profile/profile';
-import { ErrorsPage } from '../errors/errors';
-import { ConfirmPage } from '../confirm/confirm';
-import { InstitutionPage } from '../institution/institution';
-import { ConfigPage } from '../config/config';
-import { CatflowPage } from '../catflow/catflow';
-import { AuthPage } from '../auth/auth';
 
 @Component({
   selector: 'page-welcome',
@@ -16,33 +8,6 @@ import { AuthPage } from '../auth/auth';
 export class WelcomePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  // TODO: REMOVE THIS NAVIGATE, AFTER IMPLEMENTS NAVIGATION FROM PAGES
-  async navigateTo(page: string) {
-    switch (page) {
-      case 'profile':
-        await this.navCtrl.push(ProfilePage);
-        break;
-      case 'error':
-        await this.navCtrl.push(ErrorsPage);
-        break;
-      case 'confirm':
-        await this.navCtrl.push(ConfirmPage);
-        break;
-      case 'config':
-        await this.navCtrl.push(ConfigPage);
-        break;
-      case 'institution':
-        await this.navCtrl.push(InstitutionPage);
-        break;
-      case 'catflow':
-        await this.navCtrl.push(CatflowPage);
-        break;
-      case 'auth':
-        await this.navCtrl.push(AuthPage);
-        break;
-    }
   }
 
 }
