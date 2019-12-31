73d6f5a91edaa5a5d19c482debe8ce5b30aa4e14
CarlosFJ <cfierro@emergya.com>
2019-12-31 15:21:26 +0100

    Storage certs

--- src/src/pages/profile/profile.ts
+++ src/src/pages/profile/profile.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { WifiConfirmation } from '../wifiConfirmation/wifiConfirmation';
-import {GeteduroamServices} from "../../providers/geteduroam-services/geteduroam-services";
-import {isArray, isObject} from "ionic-angular/util/util";
-import {AuthenticationMethod} from "../../shared/entities/authenticationMethod";
-import {ErrorHandlerProvider} from "../../providers/error-handler/error-handler";
-import {LoadingProvider} from "../../providers/loading/loading";
+import { GeteduroamServices } from '../../providers/geteduroam-services/geteduroam-services';
+import { isArray, isObject } from 'ionic-angular/util/util';
+import { AuthenticationMethod } from '../../shared/entities/authenticationMethod';
+import { ErrorHandlerProvider } from '../../providers/error-handler/error-handler';
+import { LoadingProvider } from '../../providers/loading/loading';
+import { FilesystemDirectory, FilesystemEncoding, Plugins } from '@capacitor/core';
 
+const { Filesystem } = Plugins;
 
 
 @Component({
@@ -32,10 +34,14 @@ export class ProfilePage implements OnInit{
    */
   authenticationMethods: AuthenticationMethod[];
 
+  model = {name: '', pass: ''};
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private getEduroamServices: GeteduroamServices, private errorHandler: ErrorHandlerProvider,
               public loading: LoadingProvider) {
 
+    console.log(this.getEapconfigEndpoint);
 
   }
 
@@ -44,12 +50,16 @@ export class ProfilePage implements OnInit{
     this.showAll = false;
     await this.navCtrl.push(WifiConfirmation, {}, {animation: 'transition'});
   }
+  checkForm() {
+    console.log(this.model);
+  }
 
   /**
    * Method which returns the eap institutionSearch endpoint
    * @return {any} eapconfig_endpoint the eap institutionSearch endpoint
    */
   getEapconfigEndpoint(){
+
     return this.profile.eapconfig_endpoint;
   }
 
@@ -63,11 +73,12 @@ export class ProfilePage implements OnInit{
     this.loading.createAndPresent();
     this.profile = this.navParams.get('profile');
     console.log(this.profile);
-  /*
+
     this.eapConfig = await this.getEduroamServices.getEapConfig(this.profile.eapconfig_endpoint);
     let validEap: boolean = await this.validateEapconfig();
 
     if (validEap) {
+      await this.storageFile(this.eapConfig);
       this.getFirstValidAuthenticationMethod();
       console.log('Fist valid authentication method', this.getFirstValidAuthenticationMethod());
     } else {
@@ -75,10 +86,63 @@ export class ProfilePage implements OnInit{
       // await this.navCtrl.pop();
     }
 
-   */
     this.loading.dismiss();
     this.showAll = true;
   }
+
+  async storageFile(file) {
+
+    const fileCert = JSON.stringify(file);
+    console.log('This is a cert: ', fileCert);
+
+    try {
+      await Filesystem.mkdir({
+        createIntermediateDirectories: true,
+        path: 'certs',
+        directory: FilesystemDirectory.Documents,
+        recursive: true
+      });
+
+      await Filesystem.writeFile({
+        path: 'certs/eap-cert.eap-config',
+        data: fileCert,
+        directory: FilesystemDirectory.Documents,
+        encoding: FilesystemEncoding.UTF8
+      });
+
+      await Filesystem.readFile({
+        path: 'certs/eap-cert.eap-config',
+        directory: FilesystemDirectory.Documents,
+        encoding: FilesystemEncoding.UTF8
+      });
+
+      await Filesystem.readdir({
+        path: 'certs',
+        directory: FilesystemDirectory.Documents
+      });
+
+      await Filesystem.appendFile({
+        path: 'certs/eap-cert.eap-config',
+        data: fileCert,
+        directory: FilesystemDirectory.Documents,
+        encoding: FilesystemEncoding.UTF8
+      });
+
+      let uri = await Filesystem.getUri({
+        path: 'certs/eap-cert.eap-config',
+        directory: FilesystemDirectory.Documents,
+      });
+
+      console.log("Success save file in " + uri.uri);
+      //uri.uri -> file:///var/mobile/Containers/Data/Application/
+      //  A7CE396B-9C54-4F7D-96EF-9004E5AE885A/Documents/certs/eap-cert.eap-config
+
+    } catch(e) {
+
+      console.error('Unable to write file', e);
+    }
+
+  };
 
   /**
    * Method to validate the eapconfig file and obtain its elements.
@@ -129,7 +193,7 @@ export class ProfilePage implements OnInit{
     } else {
       return false;
     }
-
+    console.log('authentication: ',this.authenticationMethods);
     return true;
   }
 
@@ -156,11 +220,5 @@ export class ProfilePage implements OnInit{
    */
   ionViewDidEnter() {
     this.showAll = true;
-  }
-  /**
-   * Method executed when the class leave view to the next page
-   */
-  ionViewDidLeave() {
-    this.showAll = false;
   }
 }
