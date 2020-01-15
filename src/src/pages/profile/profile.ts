1ae1c6de6e1ee22b3b0f55f5105c4122408d9edd
Cardeniosa <julioap@gmail.com>
2020-01-15 09:41:08 +0100

    coneccting by using the plugin

--- src/src/pages/profile/profile.ts
+++ src/src/pages/profile/profile.ts
@@ -13,6 +13,8 @@ import {ValidatorProvider} from "../../providers/validator/validator";
 import {Plugins} from "@capacitor/core";
 import { ProfileModel } from '../../shared/models/profile-model';
 import { ProvideModel } from '../../shared/models/provide-model';
+declare var Capacitor;
+const { WifiEapConfigurator } = Capacitor.Plugins;
 const {Browser} = Plugins;
 
 @Component({
@@ -81,22 +83,29 @@ export class ProfilePage implements OnInit{
 
       console.log('before connecting');
 
-      console.log('this.provide.email', this.provide.email);
-      console.log('this.provide.pass', this.provide.pass);
-      console.log('this.authenticationMethods[0].eapMethod.type', this.authenticationMethods[0].eapMethod.type);
-      console.log('this.authenticationMethods[0].serverSideCredential.serverID', this.authenticationMethods[0].serverSideCredential.serverID);
-      console.log('this.authenticationMethods[0].serverSideCredential.ca', this.authenticationMethods[0].serverSideCredential.ca);
+      console.log('ssid', "eduroam");
+      console.log('username', this.provide.email);
+      console.log('password', this.provide.pass);
+      // console.log('eap', parseInt(''+this.authenticationMethods[0].eapMethod.type));
+      console.log('eap', parseInt(''+21));
+      console.log('servername', "");
+      console.log('auth', parseInt(''+4));
+      console.log('anonymous', "");
+      // console.log('caCertificate', this.authenticationMethods[0].serverSideCredential.ca.content);
+      console.log('caCertificate', "");
 
 
       let config = {
         ssid: "eduroam",
         username: this.provide.email,
         password: this.provide.pass,
-        eap:  this.authenticationMethods[0].eapMethod.type,
-        servername: this.authenticationMethods[0].serverSideCredential.serverID,
+        // eap: parseInt(''+this.authenticationMethods[0].eapMethod.type),
+        eap: 21,
+        servername: "", //TODO for now empty
         auth: 4,
-        anonymous: "", //TODO check if this must be empty or where is this info available
-        caCertificate: this.authenticationMethods[0].serverSideCredential.ca
+        anonymous: "", //TODO for now empty
+        // caCertificate: this.authenticationMethods[0].serverSideCredential.ca.content
+        caCertificate: ""
       };
       /*
         {
@@ -110,7 +119,13 @@ export class ProfilePage implements OnInit{
           caCertificate: "MIIEbzCCA1egAwIBAgIJAJAhu7l6dg+nMA0GCSqGSIb3DQEBBQUAMEoxCzAJBgNVBAYTAk5PMRMwEQYDVQQKEwpVTklORVRUIEFTMSYwJAYDVQQDEx1VTklORVRUIENlcnRpZmljYXRlIEF1dGhvcml0eTAeFw0xMDAyMDYwMDEyMzBaFw0yMDAyMDQwMDEyMzBaMEoxCzAJBgNVBAYTAk5PMRMwEQYDVQQKEwpVTklORVRUIEFTMSYwJAYDVQQDEx1VTklORVRUIENlcnRpZmljYXRlIEF1dGhvcml0eTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAK2+21jlJLycaCgg6TBo+i37DkWvW4UR3ptLzQAQfBuOSfPBPG9zXhmn0z/gNWfpbAwETiW+2oTcSKz/XJ0Ej1dFnySNWBnNb6rOY7GrTAvkRfDbpacQATPwg9RnvBs4xR+6TGNLcYjcyEnjF+Xd29aRzH/rFkJHq2pM6rT5BpScQ4n1DrB2y+E812UjDYhx8KnD9Zh+83wpa3tMRI5J9n7AuqrBThS4xudCAcJLMyu3KTEnBpRMRfduVyndPTJe+EVcp3XBip41Biza73ZFScqMDFfskc2jT3XV3Tz+0Actg56m+JirRtcQc8lP7o/P6BXTRmIfeXbHuX7/BSE+AXECAwEAAaOCAVYwggFSMB0GA1UdDgQWBBQlxqCOiIgff64MlbIUojA2QgTzTjB6BgNVHSMEczBxgBQlxqCOiIgff64MlbIUojA2QgTzTqFOpEwwSjELMAkGA1UEBhMCTk8xEzARBgNVBAoTClVOSU5FVFQgQVMxJjAkBgNVBAMTHVVOSU5FVFQgQ2VydGlmaWNhdGUgQXV0aG9yaXR5ggkAkCG7uXp2D6cwDAYDVR0TBAUwAwEB/zAbBgNVHREEFDASgRBkcmlmdEB1bmluZXR0Lm5vMDgGA1UdHwQxMC8wLaAroCmGJ2h0dHA6Ly9jYS51bmluZXR0Lm5vL3VuaW5ldHQtY2EtY3JsLnBlbTAzBggrBgEFBQcBAQQnMCUwIwYIKwYBBQUHMAGGF2h0dHA6Ly9vY3NwLnVuaW5ldHQubm8vMBsGA1UdEgQUMBKBEGRyaWZ0QHVuaW5ldHQubm8wDQYJKoZIhvcNAQEFBQADggEBAA9/27nksOl8d7uwi8Ce0u8WOpwDnwUUdYu0/1U91bG+bVxFL/rmenLVJJ9vaU0jxa/xHG2r8Q1RvIz1OqGX8XpbzB9cIB2Bj4kIJ+wg+pHroH9hmhJkf1gxMphtcZL3B2KAAc1B27ZchEJifFJuvL+wghAWVh0iwxhul5JOgDH0cXwvNyjRJjR70uvpU2YmRhNunqhU6hd89HPZpSybq5LU939i5HSnSgAsqQmOSCt0APlJNlJ/y5UWxMBO9ayycIuSHbORBJ8ZnXHw3yScbIEioqvAaDJNQUTNw8Pnn/dq6ffTELCFs/4QBOz7av0IxjnemYuCzgUZmb+YPhYKW+c="
         }
       */
-      const checkRequest = this.getEduroamServices.connectProfile(config);
+
+
+      // const checkRequest = this.getEduroamServices.connectProfile(config);
+
+      const checkRequest = WifiEapConfigurator.configureAP(config).then().catch(async (e) => {
+        await this.errorHandler.handleError(e.message, false);
+      });
 
       if (!!checkRequest) {
         this.navigateTo();
