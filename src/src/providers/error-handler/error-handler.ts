031ced7c25b3a16abd0aa6efdf74d9e5d3527112
CarlosFJ <cfierro@emergya.com>
2020-02-17 12:47:14 +0100

    Comment source code error service

--- src/src/providers/error-handler/error-handler.ts
+++ src/src/providers/error-handler/error-handler.ts
@@ -12,17 +12,21 @@ export class ErrorHandlerProvider extends ErrorHandler {
 
   }
 
+  /**
+   * Method to handler error
+   * @param errorText: String error message
+   * @param isFinal: Boolean (Optional)
+   * @param helpDeskUrl: String url to link (Optional)
+   * @param method: String (Optional)
+   */
   async handleError(errorText: string, isFinal?:boolean, helpDeskUrl?:string, method?:string) {
 
-  //  errorText = errorText.includes('associated') ? errorText + ".\nPlease delete the eduroam network stored in 'Saved networks'" : errorText;
-
-
-    // let localNavigation: boolean = navigation == undefined ? true : navigation;
     let localFinal: boolean = !!isFinal ? isFinal : false;
     let localUrl:string = !!helpDeskUrl ? helpDeskUrl : '';
     let localMethod:string = !!method ? method : '';
     let errorModal = this.modalCtrl.create(ErrorsPage, {error: errorText, isFinal: localFinal, link: localUrl, method: localMethod});
-    if ( !this.showModal ) {
+
+    if (!this.showModal) {
       this.showModal = true;
 
       return await errorModal.present();
