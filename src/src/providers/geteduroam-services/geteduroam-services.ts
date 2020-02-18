ea27b0ec933c8dc93967b223f42083b80b6a1a0b
CarlosFJ <cfierro@emergya.com>
2020-02-18 13:02:42 +0100

    Fix error on uri

--- src/src/providers/geteduroam-services/geteduroam-services.ts
+++ src/src/providers/geteduroam-services/geteduroam-services.ts
@@ -110,8 +110,8 @@ export class GeteduroamServices {
    */
   async generateOAuthFlow(data: oAuthModel) {
 
-    let url = `${data.oAuthUrl}?client_id=${data.client_id}&response_type=${data.type}&redirect_uri=${data.redirectUrl}
-               &scope=${data.scope}&state=${CryptoUtil.generateRandomString(10)}`;
+    let url = `${data.oAuthUrl}?client_id=${data.client_id}&response_type=${data.type}&redirect_uri=${data.redirectUrl}`
+        url += `&scope=${data.scope}&state=${CryptoUtil.generateRandomString(10)}`;
     let codeVerifier = CryptoUtil.generateRandomString(43);
     let codeChallenge = await CryptoUtil.deriveChallenge(codeVerifier);
 
