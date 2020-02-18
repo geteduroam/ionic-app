2a5a3d104ef7c6cdbd357c24dc7b20169c27c033
CarlosFJ <cfierro@emergya.com>
2020-02-18 10:32:55 +0100

    Comment source code validator service

--- src/src/providers/validator/validator.ts
+++ src/src/providers/validator/validator.ts
@@ -1,19 +1,21 @@
 import {Injectable} from "@angular/core";
 import {Validators} from "@angular/forms";
-import {isArray, isObject} from "ionic-angular/util/util";
-import {ProviderInfo} from "../../shared/entities/providerInfo";
-import {AuthenticationMethod} from "../../shared/entities/authenticationMethod";
 
 @Injectable()
 export class ValidatorProvider extends Validators {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    validateEmail(email: string, suffix?: string): boolean{
-        let regExpEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /**
+   * Method to validate email and it checks if contain identity suffix
+   * @param email
+   * @param suffix
+   */
+  validateEmail(email: string, suffix?: string): boolean{
+    let regExpEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        return !!suffix ? regExpEmail.test(String(email).toLowerCase()) && email.includes(suffix) :
-          regExpEmail.test(String(email).toLowerCase());
-    }
+    return !!suffix ? regExpEmail.test(String(email).toLowerCase()) && email.includes(suffix) :
+      regExpEmail.test(String(email).toLowerCase());
+  }
 }
