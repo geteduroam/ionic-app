b4dea6f54d19d0932233ceef4b6ee40910ca37b7
CarlosFJ <cfierro@emergya.com>
2020-09-24 08:35:32 +0200

    Realm added on username

--- src/src/pages/profile/profile.ts
+++ src/src/pages/profile/profile.ts
@@ -98,8 +98,8 @@ export class ProfilePage extends BasePage{
   /**
    * Method to show dynamically identity institution on email input
    */
-  getEmail() {
-    if (!!this.provide.email && !this.provide.email.includes('@') && !!this.suffixIdentity) {
+  getRealmEmail() {
+    if (!!this.provide.email && !this.provide.email.includes('@') && !!this.suffixIdentity && !!this.hintIdentity) {
       this.provide.email = `${this.provide.email}@${this.suffixIdentity}`;
     }
   }
@@ -180,13 +180,11 @@ export class ProfilePage extends BasePage{
 
   /**
    * Method to validate email.
-   * @return {git boolean}
+   * @return {boolean}
    */
   validEmail(email: string) {
     if (!!this.suffixIdentity && email !== '') {
       this.validMail = this.validator.validateEmail(email, this.suffixIdentity);
-    } else if (this.suffixIdentity === '' && email !== '') {
-      this.validMail = this.validator.validateEmail(email);
     }
   }
 
@@ -195,15 +193,15 @@ export class ProfilePage extends BasePage{
    * @param email
    */
   checkSuffix(email: string) {
-    if (!!this.suffixIdentity && this.suffixIdentity !== '' && email !== '' && !!this.hintIdentity) {
-      this.validSuffix = email.includes('@' + this.suffixIdentity);
-    }else if (!!this.suffixIdentity && this.suffixIdentity !== '' && email !== '' && !this.hintIdentity) {
+    if (!!this.suffixIdentity && this.suffixIdentity !== '' &&  email !== '' && !!this.hintIdentity) {
+      this.validSuffix = email.includes(`@${this.suffixIdentity}`);
+    } else if (!!this.suffixIdentity && this.suffixIdentity !== '' && email !== '' && !this.hintIdentity) {
       this.validSuffix = email.includes(this.suffixIdentity);
     }
   }
 
   blur() {
-    // this.getEmail();
+    this.getRealmEmail();
     this.checkSuffix(this.provide.email);
     this.validateForm();
   }
@@ -225,6 +223,8 @@ export class ProfilePage extends BasePage{
 
       if (!!this.validMethod.clientSideCredential.innerIdentityHint) {
         this.hintIdentity = (this.validMethod.clientSideCredential.innerIdentityHint === 'true');
+      } else {
+        this.hintIdentity = false;
       }
 
     } else {
