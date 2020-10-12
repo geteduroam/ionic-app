0ef47b6cf7d3ddadcb053328fb9ad51e9e28b88e
amachadogarcia <amachado@emergya.com>
2020-10-12 12:38:21 +0200

    Fixed error treatment when get network error

--- src/src/pages/profile/profile.ts
+++ src/src/pages/profile/profile.ts
@@ -132,7 +132,7 @@ export class ProfilePage extends BasePage{
         await this.errorHandler.handleError(
             this.dictionary.getTranslation('error', 'available1') + this.global.getSsid() +
             this.dictionary.getTranslation('error', 'available2') +
-            this.global.getSsid() + '.', false, '', 'removeConnection', true);
+            this.global.getSsid() + '.', false, '', '', true);
       } else if (checkRequest.message.includes('error.network.userCancelled')) {
         this.showAll = true;
       } else {
