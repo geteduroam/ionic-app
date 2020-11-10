d913448486a25410342be8982ee7854842bddaab
CarlosFJ <cfierro@emergya.com>
2020-11-10 12:11:28 +0100

    oAuthFlow MacCatalyst

--- src/src/pages/oauthFlow/oauthFlow.ts
+++ src/src/pages/oauthFlow/oauthFlow.ts
@@ -3,15 +3,15 @@ import {Events, NavController, NavParams} from 'ionic-angular';
 import { LoadingProvider } from '../../providers/loading/loading';
 import { ProfileModel } from '../../shared/models/profile-model';
 import { GeteduroamServices } from '../../providers/geteduroam-services/geteduroam-services';
-import { HTTP } from '@ionic-native/http/ngx';
 import {BasePage} from "../basePage";
 import {DictionaryServiceProvider} from "../../providers/dictionary-service/dictionary-service-provider.service";
 import {GlobalProvider} from "../../providers/global/global";
 import {ProviderInfo} from "../../shared/entities/providerInfo";
 import {ErrorHandlerProvider} from "../../providers/error-handler/error-handler";
 import {OauthConfProvider} from "../../providers/oauth-conf/oauth-conf";
-import { Plugins } from '@capacitor/core';
-const { OAuth2Client } = Plugins;
+import '@capacitor-community/http';
+import { AppUrlOpen, Plugins } from '@capacitor/core';
+const { OAuth2Client, Device, App, Http, Browser } = Plugins;
 
 @Component({
   selector: 'page-oauthFlow',
@@ -28,16 +28,11 @@ export class OauthFlow extends BasePage{
   profile: ProfileModel;
 
   /**
-   * This provide the url to get a token
-   */
-  tokenURl: any;
-
-  /**
    * Provide info from a certificate
    */
   providerInfo: ProviderInfo = new ProviderInfo();
 
-  constructor(private http: HTTP, public navCtrl: NavController, public navParams: NavParams, protected loading: LoadingProvider,
+  constructor(public navCtrl: NavController, public navParams: NavParams, protected loading: LoadingProvider,
               private getEduroamServices: GeteduroamServices, protected dictionary: DictionaryServiceProvider, protected event: Events,
               protected global: GlobalProvider, private errorHandler: ErrorHandlerProvider)  {
     super(loading, dictionary, event, global);
@@ -48,26 +43,73 @@ export class OauthFlow extends BasePage{
    * Method to open browser and initialize the oAuth flow
    * [Api Documentation]{@link https://github.com/Uninett/lets-wifi/blob/master/API.md#authorization-endpoint}
    */
-  buildFlowAuth() {
-    OAuth2Client.authenticate({
+  async buildFlowAuth() {
+    const device = await Device.getInfo();
+    const optionsOAuth = {
       authorizationBaseUrl: this.profile.authorization_endpoint,
       accessTokenEndpoint: this.profile.token_endpoint,
       scope: 'eap-metadata',
       pkceEnabled: true,
-
       appId: this.global.getClientId(),
       responseType: 'code',
       redirectUrl: this.global.getClientId() + ':/',
-    }).then(resourceUrlResponse => {
-      console.log('res oAuth: ', resourceUrlResponse);
+    };
 
+    if (device.name.indexOf('Mac') === -1) {
+
+    OAuth2Client.authenticate(optionsOAuth).then(resourceUrlResponse => {
       this.handleOAuthResponse(resourceUrlResponse);
-
-    }).catch(reason => {
-      this.closeEventBrowser(reason);
+    }).catch(async (reason) => {
+        this.closeEventBrowser(reason);
     });
   }
+  else {
+    this.macCatalystFlow(optionsOAuth);
+}
+  }
 
+  async macCatalystFlow(options: any ) {
+    let oAuth = await this.getEduroamServices.generateOAuthFlow(options);
+
+    await Browser.open({url: `${this.profile.authorization_endpoint}?client_id=${this.global.getClientId()}&redirect_uri=${this.global.getClientId()}:/&response_type=code&scope=eap-metadata&code_challenge=${oAuth.codeChallenge}&code_challenge_method=S256&state=${oAuth.state}`})
+    App.addListener('appUrlOpen', async (urlOpen: AppUrlOpen) => {
+
+      if (urlOpen.url.indexOf('&code=') == -1) {
+        console.log('app: ', App.getState());
+        let urlData = urlOpen.url.split('code=')[1];
+        let arrayData = urlData.split('&state=');
+        let headers = {'Content-Type':'application/x-www-form-urlencoded'};
+        let code = arrayData[0];
+        let state = arrayData[1];
+        let data = {
+          'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': options.redirectUrl,
+            'client_id': this.global.getClientId(),
+            'code_verifier': oAuth.codeVerifier,
+        };
+        const response = await Http.request({
+          method: 'POST',
+          url: this.profile.token_endpoint,
+          headers,
+          data
+        });
+        console.log(response.data);
+        this.profile.token = response.data.access_token;
+
+        const validProfile:boolean = await this.getEduroamServices.eapValidation(this.profile);
+        const oauthConf: OauthConfProvider = new OauthConfProvider(this.global, this.getEduroamServices, this.loading, this.errorHandler, this.dictionary, this.navCtrl);
+        this.providerInfo = this.global.getProviderInfo();
+        await oauthConf.manageProfileValidation(validProfile, this.providerInfo);
+      }
+    });
+
+    /*
+
+    this.buildFlowAuth(oAuth, oauth2Options, this.profile.token_endpoint);
+
+     */
+  }
 
   closeEventBrowser(error?: boolean) {
     this.loading.create();
@@ -84,6 +126,7 @@ export class OauthFlow extends BasePage{
    * @param res url to get token
    */
   async handleOAuthResponse(res) {
+    console.log('handleOAuthResponse: ', res);
     this.profile.token = res['access_token'];
 
     const validProfile:boolean = await this.getEduroamServices.eapValidation(this.profile);
