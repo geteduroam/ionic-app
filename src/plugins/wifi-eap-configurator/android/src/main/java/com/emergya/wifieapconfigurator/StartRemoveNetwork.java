80c3e5b031434a61a8b8eb0f9fceadacf4c04807
amachadogarcia <amachado@emergya.com>
2020-11-30 13:35:43 +0100

    Modified service to removeNetwork

--- src/plugins/wifi-eap-configurator/android/src/main/java/com/emergya/wifieapconfigurator/StartRemoveNetwork.java
+++ src/plugins/wifi-eap-configurator/android/src/main/java/com/emergya/wifieapconfigurator/StartRemoveNetwork.java
@@ -25,21 +25,23 @@ public class StartRemoveNetwork extends JobIntentService {
     protected void onHandleWork(@NonNull Intent intent) {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        String date = sharedPref.getString("date", "");
-        int netId = sharedPref.getInt("netId", -1);
-        String fqdn = sharedPref.getString("fqdn", "");
-        Long dateUntil = Long.parseLong(date);
-        Date dateNow = new Date();
-        Long millisNow = dateNow.getTime();
-        Long delay = dateUntil - millisNow;
-        if ( delay.compareTo(Long.valueOf(0)) > 0 ) {
-            AlarmManager mgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-            Intent i = new Intent(getApplicationContext(), NotificationReceiver.class);
-            i.putExtra("expiration", true);
-            i.putExtra("netId", netId);
-            i.putExtra("fqdn", fqdn);
-            PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 1, i, 0);
-            mgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + delay, pi);
+        if (sharedPref.getString("date", "") != "") {
+            String date = sharedPref.getString("date", "");
+            int netId = sharedPref.getInt("netId", -1);
+            String fqdn = sharedPref.getString("fqdn", "");
+            Long dateUntil = Long.parseLong(date);
+            Date dateNow = new Date();
+            Long millisNow = dateNow.getTime();
+            Long delay = dateUntil - millisNow;
+            if (delay.compareTo(Long.valueOf(0)) > 0) {
+                AlarmManager mgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+                Intent i = new Intent(getApplicationContext(), NotificationReceiver.class);
+                i.putExtra("expiration", true);
+                i.putExtra("netId", netId);
+                i.putExtra("fqdn", fqdn);
+                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 1, i, 0);
+                mgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + delay, pi);
+            }
         }
     }
 
