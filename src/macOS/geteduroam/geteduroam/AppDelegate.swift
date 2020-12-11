fa10aafda1cdf8c38834e3ec6fe5a0402baf25a7
CarlosFJ <cfierro@emergya.com>
2020-12-11 17:16:34 +0100

    Create EAPView storyboard

--- src/macOS/geteduroam/geteduroam/AppDelegate.swift
+++ src/macOS/geteduroam/geteduroam/AppDelegate.swift
@@ -9,16 +9,21 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
+   
+    var eapObject: EAP? = nil
+
     func application(_ sender: NSApplication, openFile filename: String) -> Bool {
         // Parsing file
         let string = try! String(contentsOfFile: filename, encoding: String.Encoding.utf8)
-        print(string)
-        
+
         // Class EAP defining eapObject
-        let eapObject = EAP(XMLString: string)
+        eapObject = EAP(XMLString: string)
         
-        // TODO: Catch error when parsing eap file thrown a nil value
+        let mainStoryboard : NSStoryboard = NSStoryboard(name: "Main", bundle: nil)
+        let initialViewController : NSViewController = mainStoryboard.instantiateController(identifier: "EAPView")
+        self.window = NSWindow(frame: NSScreen.main, )
+        self.window?.rootViewController = initialViewController
+               self.window?.makeKeyAndVisible()
         
         print("Data extract")
         print("----------------")
@@ -27,9 +32,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         print("eapType: ", eapObject?.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod?.first?.EAPMethod?.EapType.eap as Any)
         print("ServerID: ", eapObject?.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod?.first?.ServerSideCredential.ServerID as Any)
         print("CA: ", eapObject?.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod?.first?.ServerSideCredential?.CA?.first?.CACert as Any)
+        
         return true
     }
-
+    
+    func getEAPConfig() -> EAP? {
+        if (eapObject != nil) {
+            return eapObject
+        } else {
+            return nil
+        }
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
