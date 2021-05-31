d8c971639be25e418e72842e3d0719c7576280d5
Paul Dekkers <paul.dekkers@surf.nl>
2021-05-31 20:51:44 +0200

    Use root CA certificate for Passpoint and not first leaf certificate
    This reverts commit df5d93846cd0ad1d56462a5088c2ca12fd24ce62.

--- geteduroam/plugins/wifi-eap-configurator/android/src/main/java/com/emergya/wifieapconfigurator/config/WifiProfile.java
+++ geteduroam/plugins/wifi-eap-configurator/android/src/main/java/com/emergya/wifieapconfigurator/config/WifiProfile.java
@@ -451,22 +451,17 @@ public class WifiProfile {
 		return enterpriseConfig;
 	}
 
-	protected final List<X509Certificate> getLeafCaCertificates() throws WifiEapConfiguratorException {
-		List<X509Certificate> leafCertificates = new ArrayList<>(caCertificates.size());
+	protected final List<X509Certificate> getRootCaCertificates() throws WifiEapConfiguratorException {
+		List<X509Certificate> rootCertificates = new ArrayList<>(caCertificates.size());
 
-		c1:
-		for (X509Certificate c1 : caCertificates) {
-			for (X509Certificate c2 : caCertificates) {
-				if (c1 == c2) continue;
-
-				// If c2 has c1 as issuer, c1 is not a leaf
-				if (c1.getSubjectDN().equals(c2.getIssuerDN())) continue c1; // try next c1
+		for (X509Certificate c : caCertificates) {
+			System.err.println();
+			if (c.getSubjectDN().toString().equals(c.getIssuerDN().toString())) {
+				rootCertificates.add(c);
 			}
-
-			leafCertificates.add(c1);
 		}
 
-		return leafCertificates;
+		return rootCertificates;
 	}
 
 	/**
@@ -499,13 +494,15 @@ public class WifiProfile {
 		passpointConfig.setHomeSp(homeSp);
 
 		Credential cred = new Credential();
-		List<X509Certificate> rootCertificates = getLeafCaCertificates();
+		List<X509Certificate> rootCertificates = getRootCaCertificates();
 		// TODO Add support for multiple CAs
-		assert (rootCertificates.size() != 0);
-		if (rootCertificates.size() != 1) {
-			Log.e(getClass().getSimpleName(), "Passpoint configuration may not work due to too many CAs in the profile (1 supported, " + rootCertificates.size() + " given)");
+		if (rootCertificates.size() == 1) {
+			// Just use the first CA for Passpoint
+			cred.setCaCertificate(rootCertificates.get(0));
+		} else {
+			Log.e(getClass().getSimpleName(), "Not creating Passpoint configuration due to too many CAs in the profile (1 supported, " + getRootCaCertificates().size() + " given)");
+			return null;
 		}
-		cred.setCaCertificate(rootCertificates.get(0));
 		// TODO Set server name check somehow
 		cred.setRealm(fqdn);
 
